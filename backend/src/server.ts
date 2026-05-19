283dc0d35fb93b1c6553647f706438bad499bd43
161seconds <quocbao.nguyen16102006@gmail.com>
2026-05-19 23:11:52 +0700

    refactor: remove redundant allowedOrigins variable in CORS configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,11 +20,6 @@ const PORT = process.env.PORT || 5000
 
 initFolder()
 
-const allowedOrigins = [
-    'http://localhost:5173',
-    'https://accrue-merit.vercel.app'
-]
-
 app.use(cors({
     origin: [
         'https://accrue-merit.vercel.app',
